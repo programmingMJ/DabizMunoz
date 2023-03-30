17eb27dfa91310d11de23d44db05f56f024193b9
Maria José Esquinas Esquinas <marijose3946@gmail.com>
2023-03-30 16:28:29 +0200

    Inicial

---
+++ app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
@@ -0,0 +1,11 @@
+package com.example.by_dabiz_munoz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Dishes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dishes)
+    }
+}
