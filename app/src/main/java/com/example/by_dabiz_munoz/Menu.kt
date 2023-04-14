0ffcf752955d3f8526bcad905bd2675673830b18
Maria José Esquinas Esquinas <marijose3946@gmail.com>
2023-04-14 15:59:46 +0200

    botones

--- app/src/main/java/com/example/by_dabiz_munoz/Menu.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Menu.kt
@@ -1,11 +1,24 @@
 package com.example.by_dabiz_munoz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val dishes: Button = findViewById(R.id.Dishes_button)
+        val recipes: Button = findViewById(R.id.Dishes_button)
+
+        dishes.setOnClickListener{
+            startActivity(Intent(this, Dishes::class.java))
+        }
+
+        recipes.setOnClickListener{
+            startActivity(Intent(this, Recipes::class.java))
+        }
     }
+
 }
