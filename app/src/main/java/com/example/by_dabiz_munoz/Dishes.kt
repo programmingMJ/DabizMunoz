9e2345c4a3282a0813b22cd001bf997eecc28800
Maria José Esquinas Esquinas <marijose3946@gmail.com>
2023-04-20 10:16:45 +0200

    funciona el botón menú

--- app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
@@ -1,11 +1,19 @@
 package com.example.by_dabiz_munoz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Dishes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dishes)
+        val button_home: FloatingActionButton = findViewById(R.id.floatingActionButton_house)
+        button_home.setOnClickListener{
+            startActivity(Intent(this, Menu::class.java))
+        }
+
     }
 }
