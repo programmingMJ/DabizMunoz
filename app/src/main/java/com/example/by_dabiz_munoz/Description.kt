038f88e39dc11768a09c5ef9d5bcde2392c291dc
programmingHelen <128732393+programmingHelen@users.noreply.github.com>
2023-04-21 18:41:01 +0200

    lista

--- app/src/main/java/com/example/by_dabiz_munoz/Description.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Description.kt
@@ -2,10 +2,21 @@ package com.example.by_dabiz_munoz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.by_dabiz_munoz.databinding.ActivityDescriptionBinding
 
 class Description : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDescriptionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDescriptionBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_description)
+
+        val title = intent.getStringExtra("title")
+        val imageId = intent.getIntExtra("imageId",R.drawable.carrilleras)
+
+        binding.title.text = title
+        binding.imageFood.setImageResource(imageId)
+
     }
 }
--- app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
@@ -3,13 +3,13 @@ package com.example.by_dabiz_munoz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.example.by_dabiz_munoz.databinding.ActivityDishesBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Dishes : AppCompatActivity() {
 
     private lateinit var binding: ActivityDishesBinding
+    private lateinit var plateArrayList : ArrayList<Plate>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDishesBinding.inflate(layoutInflater)
@@ -23,13 +23,23 @@ class Dishes : AppCompatActivity() {
         plateArrayList = ArrayList()
 
         for (i in title.indices){
-            val plate = Plate(title[i],imageId[i])
+            val plate = Plate(title[i],imageId[i],"a","a")
             plateArrayList.add(plate)
         }
 
-        binding.listview.isClickable = true
-        binding.listview.adapter = MyAdapter(this,plateArrayList)
-        binding.listview.setOnClickListe
+        binding.list.isClickable = true
+        binding.list.adapter = MyAdapter(this,plateArrayList)
+        binding.list.setOnItemClickListener{ parent, view, position, id->
+
+            val title =title[position]
+            val imageId = imageId[position]
+
+            val i = Intent(this,Description::class.java)
+
+            i.putExtra("title",title)
+            i.putExtra("imageId",imageId)
+            startActivity(i)
+        }
 
 
         val button_home: FloatingActionButton = findViewById(R.id.floatingActionButton_house)
