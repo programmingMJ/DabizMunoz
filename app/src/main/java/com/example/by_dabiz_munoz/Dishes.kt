e3a5b5badae9da1685ead6baace952cf8f1fcd96
Maria José Esquinas Esquinas <marijose3946@gmail.com>
2023-04-21 16:05:22 +0200

    helen

--- app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
@@ -4,12 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.by_dabiz_munoz.databinding.ActivityDishesBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Dishes : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDishesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dishes)
+        binding = ActivityDishesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageId = intArrayOf(
+            R.drawable.carrilleras,R.drawable.rectangle_5)
+        val title = arrayOf("Carrilleras","Croquetas")
+
+
+        plateArrayList = ArrayList()
+
+        for (i in title.indices){
+            val plate = Plate(title[i],imageId[i])
+            plateArrayList.add(plate)
+        }
+
+        binding.listview.isClickable = true
+        binding.listview.adapter = MyAdapter(this,plateArrayList)
+        binding.listview.setOnClickListe
+
+
         val button_home: FloatingActionButton = findViewById(R.id.floatingActionButton_house)
         button_home.setOnClickListener{
             startActivity(Intent(this, Menu::class.java))
---
+++ app/src/main/java/com/example/by_dabiz_munoz/Plate.kt
@@ -0,0 +1,5 @@
+package com.example.by_dabiz_munoz
+
+data class Plate(var title:String,var imageId: Int,
+var description: String,var restaurant: String )
+
