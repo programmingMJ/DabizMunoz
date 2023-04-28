b70172e93336c6ead4882e6ffbc2e4091725dbc1
programmingHelen <128732393+programmingHelen@users.noreply.github.com>
2023-04-28 12:46:44 +0200

    recycler

--- app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
@@ -3,43 +3,48 @@ package com.example.by_dabiz_munoz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.by_dabiz_munoz.databinding.ActivityDishesBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class Dishes : AppCompatActivity() {
+class Dishes : AppCompatActivity(),PlateClickListener {
 
     private lateinit var binding: ActivityDishesBinding
-    private lateinit var plateArrayList : ArrayList<Plate>
+//    private lateinit var plateArrayList : ArrayList<Plate>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDishesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val imageId = intArrayOf(
-            R.drawable.carrilleras,R.drawable.rectangle_5)
-        val title = arrayOf("Carrilleras","Croquetas")
+        ourPlates()
 
-
-        plateArrayList = ArrayList()
-
-        for (i in title.indices){
-            val plate = Plate(title[i],imageId[i],"a","a")
-            plateArrayList.add(plate)
+        val dishesActivity = this
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(applicationContext,1)
+            adapter = CardAdapter(plateList,dishesActivity)
         }
 
-        binding.list.isClickable = true
-        binding.list.adapter = MyAdapter(this,plateArrayList)
-        binding.list.setOnItemClickListener{ parent, view, position, id->
-
-            val title =title[position]
-            val imageId = imageId[position]
-
-            val i = Intent(this,Description::class.java)
-
-            i.putExtra("title",title)
-            i.putExtra("imageId",imageId)
-            startActivity(i)
-        }
+//
+//        for (i in title.indices){
+//            val plate = Plate(title[i],imageId[i],"a","a")
+//            plateArrayList.add(plate)
+//        }
+//
+//        binding.list.isClickable = true
+//        binding.list.adapter = MyAdapter(this,plateArrayList)
+//        binding.list.setOnItemClickListener{ parent, view, position, id->
+//
+//            val title =title[position]
+//            val imageId = imageId[position]
+//
+//            val i = Intent(this,Description::class.java)
+//
+//            i.putExtra("title",title)
+//            i.putExtra("imageId",imageId)
+//            startActivity(i)
+//        }
 
 
         val button_home: FloatingActionButton = findViewById(R.id.floatingActionButton_house)
@@ -52,4 +57,53 @@ class Dishes : AppCompatActivity() {
             startActivity(Intent(this, Menu::class.java))
         }
     }
+    override fun onClick(plate: Plate) {
+        val intent = Intent(applicationContext,Description::class.java)
+        intent.putExtra(PLATE_ID_EXTRA,plate.id)
+        startActivity(intent)
+    }
+    private fun ourPlates(){
+        val plate1 = Plate(
+            "Carrilleras",
+            R.drawable.carrilleras,
+            "description",
+            "Diverxo"
+        )
+        plateList.add(plate1)
+        val plate2 = Plate(
+            "Carrilleras",
+            R.drawable.carrilleras,
+            "description",
+            "Diverxo"
+        )
+        plateList.add(plate2)
+        val plate3 = Plate(
+            "Carrilleras",
+            R.drawable.carrilleras,
+            "description",
+            "Diverxo"
+        )
+        plateList.add(plate3)
+
+        val plate4 = Plate(
+            "Carrilleras",
+            R.drawable.carrilleras,
+            "description",
+            "Diverxo"
+        )
+        plateList.add(plate4)
+
+        val plate5 = Plate(
+            "Carrilleras",
+            R.drawable.carrilleras,
+            "description",
+            "Diverxo"
+        )
+        plateList.add(plate5)
+
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/by_dabiz_munoz/PlateClickListener.kt
@@ -0,0 +1,6 @@
+package com.example.by_dabiz_munoz
+
+interface PlateClickListener
+{
+    fun onClick(plate: Plate)
+}
