07f59e64e0f1c1489fcef7dad2d264fbcc2bfb31
Maria José Esquinas Esquinas <marijose3946@gmail.com>
2023-05-18 17:41:15 +0200

    reproductor video

---
+++ app/src/main/java/com/example/by_dabiz_munoz/Video_repr.kt
@@ -0,0 +1,11 @@
+package com.example.by_dabiz_munoz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Video_repr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_repr)
+    }
+}
