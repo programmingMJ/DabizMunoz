f938785f3f1cc2a7de29433524d7a726e483c82b
Maria José Esquinas Esquinas <marijose3946@gmail.com>
2023-05-19 11:59:09 +0200

    va el video :p

--- app/src/main/java/com/example/by_dabiz_munoz/Video_repr.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Video_repr.kt
@@ -4,17 +4,33 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
 
 class Video_repr : AppCompatActivity() {
+    private var myVideoView: VideoView? = null
+    private var myMediaController: MediaController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_repr)
-        val videoView: VideoView = findViewById(R.id.video)
-        val mediaController =  MediaController(this)
-        mediaController.setAnchorView(videoView)
-        videoView.setMediaController(mediaController)
-        videoView.setVideoURI(Uri.parse("android.resourse://"+getPackageName()+"/"+R.raw.paella))
+        myVideoView = findViewById(R.id.video)
+        setUpVideoPlayer()
+    }
+    private fun setUpVideoPlayer() {
+        if (myMediaController == null){
+            myMediaController = MediaController(this)
+            myMediaController!!.setAnchorView(this.myVideoView)
+        }
+        myVideoView!!.setMediaController(myMediaController)
+        myVideoView!!.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.paella))
+        myVideoView!!.start()
+        myVideoView!!.setOnCompletionListener {
+            Toast.makeText(applicationContext, "Video Completed", Toast.LENGTH_SHORT).show()
+        }
+        myVideoView!!.setOnErrorListener { mp, what, extra ->
+            Toast.makeText(applicationContext, "An Error Occured", Toast.LENGTH_SHORT).show()
+            false
+        }
     }
 }
