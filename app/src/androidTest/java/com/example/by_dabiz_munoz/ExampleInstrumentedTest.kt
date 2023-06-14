95b1cb7b9fa8ddf6dd439192c7357d8dacd8caf0
programmingHelen <128732393+programmingHelen@users.noreply.github.com>
2023-06-14 18:36:54 +0200

    ESTA=¿?

--- app/src/androidTest/java/com/example/by_dabiz_munoz/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/by_dabiz_munoz/ExampleInstrumentedTest.kt
@@ -13,6 +13,8 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+///Holiiii
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
