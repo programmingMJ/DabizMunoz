b523eaf1e29fc8df02801dd2eaf0f809a382f57f
programmingHelen <128732393+programmingHelen@users.noreply.github.com>
2023-06-14 19:09:47 +0200

    Espresso

--- app/src/androidTest/java/com/example/by_dabiz_munoz/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/by_dabiz_munoz/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.example.by_dabiz_munoz
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import java.util.regex.Matcher
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -23,4 +32,69 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.by_dabiz_munoz", appContext.packageName)
     }
+
+    private val dummyPlateList: List<Plate> = listOf(
+        Plate(
+            "Crabs (Blue and Royal)",
+            R.drawable.carrilleras,
+            "Crabs (Blue and Royal) with home-fermented kimchis, sheep's yoghurt, wild strawberries and coffee",
+            R.drawable.diverxo_png
+        ),
+        Plate(
+            "Sandwich Club",
+            R.drawable.huevo,
+            "Steamed ricotta and fried quail egg club sandwich with shichimi tōgarashi.",
+            R.drawable.logo_goxo_removebg_preview
+        ),
+        Plate(
+            "Nigiri croqueta",
+            R.drawable.nigiri,
+            "sheep's milk nigiri croqueta, mille-feuille of salmon matured in jabugo fat, tomato jam and smoked tea",
+            R.drawable._92_2923622_streetxo_hd_png_download_png_removebg_preview
+        )
+    )
+
+    @Before
+    fun launchActivity() {
+        val activityScenario = ActivityScenario.launch(Dishes::class.java)
+        activityScenario.onActivity { activity ->
+            activity.runOnUiThread {
+                activity.plateList.clear()
+                activity.plateList.addAll(dummyPlateList)
+                activity.binding.recyclerView.adapter?.notifyDataSetChanged()
+            }
+        }
+    }
+
+
+    @Test
+    fun checkRecyclerViewItemVisible() {
+        // Check if the first item of RecyclerView is visible
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerView))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+//        Espresso.onView(ViewMatchers.withId(R.id.recyclerView))
+//            .check(ViewAssertions.matches(hasItemAtPosition(0)))
+    }
+
+    @Test
+    fun checkRecyclerViewItemClick() {
+        // Perform click on the first item of RecyclerView
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerView))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<CardViewHolder>(0, click()))
+    }
+
+//    private fun hasItemAtPosition(position: Int): Matcher<View> {
+//        return object : TypeSafeMatcher<View>() {
+//            override fun describeTo(description: Description) {
+//                description.appendText("has item at position $position")
+//            }
+//
+//            override fun matchesSafely(view: View): Boolean {
+//                val recyclerView = view as RecyclerView
+//                val adapter = recyclerView.adapter
+//                return adapter?.itemCount ?: 0 > position
+//            }
+//        }
+//    }
+
 }
--- app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
+++ app/src/main/java/com/example/by_dabiz_munoz/Dishes.kt
@@ -6,11 +6,13 @@ import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.by_dabiz_munoz.databinding.ActivityDishesBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.by_dabiz_munoz.CardViewHolder
 
-class Dishes : AppCompatActivity(),PlateClickListener {
+class Dishes : AppCompatActivity(), PlateClickListener {
 
-    private lateinit var binding: ActivityDishesBinding
-//    private lateinit var plateArrayList : ArrayList<Plate>
+    lateinit var binding: ActivityDishesBinding
+    public val plateList: ArrayList<Plate> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDishesBinding.inflate(layoutInflater)
@@ -21,22 +23,23 @@ class Dishes : AppCompatActivity(),PlateClickListener {
         }
         val dishesActivity = this
         binding.recyclerView.apply {
-            layoutManager = GridLayoutManager(applicationContext,1)
-            adapter = CardAdapter(plateList,dishesActivity)
+            layoutManager = GridLayoutManager(applicationContext, 1)
+            adapter = CardAdapter(plateList, dishesActivity)
         }
 
-
         val button_back: FloatingActionButton = findViewById(R.id.floatingActionButtonBack)
-        button_back.setOnClickListener{
+        button_back.setOnClickListener {
             startActivity(Intent(this, Menu::class.java))
         }
     }
+
     override fun onClick(plate: Plate) {
-        val intent = Intent(applicationContext,Description::class.java)
-        intent.putExtra(PLATE_ID_EXTRA,plate.id)
+        val intent = Intent(applicationContext, Description::class.java)
+        intent.putExtra(PLATE_ID_EXTRA, plate.id)
         startActivity(intent)
     }
-    private fun ourPlates(){
+
+    private fun ourPlates() {
         val plate1 = Plate(
             "Crabs (Blue and Royal)",
             R.drawable.carrilleras,
@@ -59,28 +62,24 @@ class Dishes : AppCompatActivity(),PlateClickListener {
         )
         plateList.add(plate3)
 
-    val plate4 = Plate(
-        "Nigiri croqueta",
-        R.drawable.nigiri,
-        "sheep's milk nigiri croqueta, mille-feuille of salmon matured in jabugo fat, tomato jam and smoked tea",
-        R.drawable._92_2923622_streetxo_hd_png_download_png_removebg_preview
-    )
-    plateList.add(plate4)
+        val plate4 = Plate(
+            "Nigiri croqueta",
+            R.drawable.nigiri,
+            "sheep's milk nigiri croqueta, mille-feuille of salmon matured in jabugo fat, tomato jam and smoked tea",
+            R.drawable._92_2923622_streetxo_hd_png_download_png_removebg_preview
+        )
+        plateList.add(plate4)
 
-    val plate5 = Plate(
-        "Sabu sabu",
-        R.drawable.sabbu,
-        "chili crab-style octopus sabu sabu with paprika de la vera and Canarian potato",
-        R.drawable._92_2923622_streetxo_hd_png_download_png_removebg_preview
-    )
-    plateList.add(plate5)
-
-
+        val plate5 = Plate(
+            "Sabu sabu",
+            R.drawable.sabbu,
+            "chili crab-style octopus sabu sabu with paprika de la vera and Canarian potato",
+            R.drawable._92_2923622_streetxo_hd_png_download_png_removebg_preview
+        )
+        plateList.add(plate5)
     }
 
     override fun onBackPressed() {
         finish()
     }
-
-
 }
